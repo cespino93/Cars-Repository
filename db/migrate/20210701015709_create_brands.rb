3eb47fddae3e39f7b9c058a4e7b4eca65420d5d3
cespino93 <chrisl.espino@gmail.com>
2021-07-01 20:25:37 -0400

    added relationships into the models

--- db/migrate/20210701015709_create_brands.rb
+++ db/migrate/20210701015709_create_brands.rb
@@ -2,7 +2,6 @@ class CreateBrands < ActiveRecord::Migration[6.1]
   def change
     create_table :brands do |t|
       t.string :name
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
