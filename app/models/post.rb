74bde63ff15f6d886a33d46259ec8c94897746d5
cespino93 <chrisl.espino@gmail.com>
2021-07-01 21:54:23 -0400

    added routes and views for signup

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :brand
   has_many :comments
-  has_many :users, through :comments
+  has_many :users, through: :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :brands
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  
+
   resources :comments
   resources :users
   resources :posts
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
