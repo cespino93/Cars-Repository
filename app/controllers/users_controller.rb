683a06e377921691dd15e9905e834bd47b639971
cespino93 <chrisl.espino@gmail.com>
2021-07-06 19:57:02 -0400

    added sessions view and home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,13 @@ class UsersController < ApplicationController
           render :new
         end
       end
-        
+         
     end
     
     def show
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user
+
     end  
 
     private
