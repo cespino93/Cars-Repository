dd65ed24423a02f6422dd7e96360b35a44a14649
cespino93 <chrisl.espino@gmail.com>
2021-07-08 22:45:14 -0400

    Final after 11 hours

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,31 @@
-Rails.application.routes.draw do
-  root "sessions#home"
-
+root "sessions#home"
+ 
+ #users route
+  get '/users/most-active' => 'users#most_active'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
-
+ 
+  #login route
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-
+ 
+  #logout route
   delete '/logout' => 'sessions#destroy'
 
+  #omniauth callback route
 
-  resources :comments
-  resources :users
-  resources :posts
+  get '/auth/google_oauth2/callback' => 'sessions#google'
+  # get '/auth/:provider/callback', => 'sessions#google'
 
+  resources :posts do
+     resources :comments
+   end
+   resources :comments
+   resources :users do
+     resources :posts, shallow: true
+   end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+   resources :categories, only: [:index, :show]
+   
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ end
